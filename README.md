546abd430dabc016148f5d8ba3911d456dc6570d
root <root@localhost.localdomain>
2020-09-18 23:09:57 +0800

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# CentOS 7.8.2003
+```
+cd libs/centos-7.8.2003
+sh install.sh
+```
+
+# Tips
+## Get OS version
+```
+# CentOS
+cat /etc/redhat-release`
+```
---
+++ scripts/install-centos-7.8.2003.sh
@@ -0,0 +1,4 @@
+rpm -ivh vim-filesystem-7.4.629-6.el7.x86_64.rpm 
+rpm -ivh vim-common-7.4.629-6.el7.x86_64.rpm 
+rpm -ivh gpm-libs-1.20.7-6.el7.x86_64.rpm 
+rpm -ivh vim-enhanced-7.4.629-6.el7.x86_64.rpm 
